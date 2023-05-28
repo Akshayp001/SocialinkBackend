961b547ea07129e64e255c49ed0b759d6d2e7b17
Akshayp001 <akshay.patil21@pccoepune.org>
2023-05-28 20:30:53 +0530

    delete post and edit post added

--- posts.routes.js
+++ posts.routes.js
@@ -70,5 +70,48 @@ app.get("/posts/:_id",async(request,response)=>{
     }
 });
 
+// Edit post caption
+app.put("/posts/edit/:_id", async (req, res) => {
+    const postId = req.params._id;
+    const { caption } = req.body;
+  
+    try {
+      const post = await postModel.findById(postId);
+  
+      if (!post) {
+        return res.status(404).json({ message: "Post not found" });
+      }
+  
+      post.caption = caption;
+      await post.save();
+  
+      res.json({ message: "Post caption updated successfully" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Server Error" });
+    }
+  });
+  
+  // Delete post by postId
+  app.delete("/posts/delete/:_id", async (req, res) => {
+    const postId = req.params._id;
+  
+    try {
+      const post = await postModel.findById(postId);
+  
+      if (!post) {
+        return res.status(404).json({ message: "Post not found" });
+      }
+  
+      await post.deleteOne();
+  
+      res.json({ message: "Post deleted successfully" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Server Error" });
+    }
+  });
+  
+
 
 module.exports=app;
