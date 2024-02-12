0004b4f7e00efffa0f19185aae365bf36b21f992
Akshayp001 <akshay.patil21@pccoepune.org>
2024-02-12 09:47:48 +0530

    correctionDone

--- server.js
+++ server.js
@@ -42,66 +42,7 @@ app.use(pRouter);
 app.use(cRouter);
 app.use(fRouter);
 
-// //check 
-// exports.instantPay = functions.https.onRequest(async (req, res) => {
-//     cors(req, res, async () => {
-//         const {
-//             country,
-//             action
-//         } = req.body;
-//     })})
-// ;
-
-app.post('/temp', async (req, res) => {
-    try {
-        const { var1, var2 } = req.body;
-        const result = var1 + var2;
-        return res.json({ result });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: 'Internal Server Error/Invalid Input' });
-    }
-});
-
 app.listen(3000,()=>{
     console.log("Server Connected On Post 3000");
 
 });
-// const paymentSchema = new mongoose.Schema({
-//     country: String,
-//     action: String,
-//     mobile_number: String,
-//     amount: Number,
-//     payment_method: Number,
-//   });
-  
-//   const Payment = mongoose.model('Payment', paymentSchema);
-  
-//   // Handle POST request to /example
-//   app.post('/instantPay', cors(), async (req, res) => {
-//     const { country, action, mobile_number, amount, payment_method } = req.body;
-  
-//     // Create a new Payment instance
-//     const newPayment = new Payment({
-//       country,
-//       action,
-//       mobile_number,
-//       amount,
-//       payment_method,
-//     });
-  
-//     try {
-//       // Save the new payment to the database
-//       const savedPayment = await newPayment.save();
-  
-//       // Log the saved payment to the console
-//       console.log('Saved payment:', savedPayment);
-  
-//       // Send a response
-//       res.status(200).json({ message: 'Data received and saved successfully' });
-//     } catch (error) {
-//       console.error('Error saving payment:', error);
-//       res.status(500).json({ message: 'Internal Server Error' });
-//     }
-//   });
-  
